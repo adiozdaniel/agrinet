875425877720ee611c44002daa1c212d7f4b69c9
adioz <adiozdaniel@gmail.com>
2025-01-10 14:32:14 +0300

    chores: add logo - For branding purpose

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,23 +1,24 @@
-import {Component, inject} from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
-import {CartService} from '../../services/cart.service';
-import {PrimaryButtonComponent} from '../primary-button/primary-button.component';
-import {NgIf} from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    RouterLink,
-    PrimaryButtonComponent,
-    NgIf
-  ],
+  imports: [RouterLink, PrimaryButtonComponent, NgIf],
   templateUrl: './header.component.html',
-  styles: ``
+  styles: `img {
+    width: 60px;
+    height: 60px;
+    transition: transform 0.9s ease;
+    transform: rotate(270deg);
+    cursor: pointer;
+  }`,
 })
 export class HeaderComponent {
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router) {}
   cartService = inject(CartService);
 
   isLandingPage(): boolean {
